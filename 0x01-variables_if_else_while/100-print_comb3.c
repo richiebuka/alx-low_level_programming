cd13604d9a4dd7a803091c07d182ee58672092f2
richiebuka <rchiebuka@gmail.com>
2023-03-17 22:59:04 +0100

    Double combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
+
 /**
- * main - print 00 to 99 witn no duplicate digits or combos: no 11, no 10 (01)
+ * main - print 00 - 99 with no duplicate digits
  * Return: 0 (Successful)
  */
 
 int main(void)
 {
-	int ones;
 	int tens;
+	int ones;
 
 	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
 	{
-		putchar(tens);
-		putchar(ones);
-		
-		if (tens != '8' || ones != '9') /*print commas*/
+		for (ones = (tens + 1); ones <= '9'; ones++) /* one is tens + 1*/
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8'; || ones != '9';) /*print commas*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
 
 	return (0);
-
 }
