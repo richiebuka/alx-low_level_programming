999b4ee40850f42952874c0409be45fb407f675e
richiebuka <rchiebuka@gmail.com>
2023-03-21 12:51:16 +0100

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,18 +3,16 @@
 /**
  * print_last_digit - prints last digit of an integer
  * @r: integer argument
- * @ld: last digit of integer
  * Return: last digit
  */
 
 int print_last_digit(int r)
 {
-	int ld = r % 10;
-	
-	if (ld < 0)
+	if (r < 0)
 	{
-		ld *= -1;
+		r *= -1;
 	}
-	_putchar('0' + ld);
-	return (0);
+	_putchar('0' + (r % 10));
+
+	return (r % 10);
 }
