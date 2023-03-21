f56bb45ec1de40e92e6414fa1d8c34bf0024404f
richiebuka <rchiebuka@gmail.com>
2023-03-21 21:33:27 +0100

    Fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print sum of even fibonacci sequence up to 4000000
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int sum = 0;
+	long int f1 = 1;
+	long int f2 = 2;
+	long int temp = 0;
+
+	while (f2 < 4000000)
+	{
+		if (f3 % 2 == 0)
+		{
+			sum += f2;
+		}
+		temp = f1;
+		fi = f2;
+		f2 = temp + f2;
+	}
+	printf("%li\n", sum);
+	return (0);
+}
