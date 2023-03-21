d452ae69a83800a9f89e43fe7c28ca55170f5c51
richiebuka <rchiebuka@gmail.com>
2023-03-21 14:43:18 +0100

    Multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,7 +39,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((product / 10) / 10 + '0');
 					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0')
+					_putchar(product % 10 + '0');
 				}
 			}
 			_putchar('\n');
