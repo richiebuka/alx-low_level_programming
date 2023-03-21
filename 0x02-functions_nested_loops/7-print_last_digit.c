785c084bea850559a53efc1704e1db89ae8f93a4
richiebuka <rchiebuka@gmail.com>
2023-03-21 13:00:42 +0100

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,11 @@ int print_last_digit(int r)
 	if (r < 0)
 	{
 		r *= -1;
-		putchar('0' + (r % 10));
+		_putchar('0' + (r % 10));
 	}
 	else
 	{
-		putchar('0' + (r % 10));
+		_putchar('0' + (r % 10));
 	}
 	return (r % 10);
 }
