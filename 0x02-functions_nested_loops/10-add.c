4ae65a9413b79ca1a4383f9cc4527fa98dad22e3
richiebuka <rchiebuka@gmail.com>
2023-03-21 13:14:56 +0100

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two integer
+ * @a: first integer argument
+ * @b: second integer argument
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
