2959fac9208367edab0ef709c354f8598f5e3eae
richiebuka <rchiebuka@gmail.com>
2023-03-23 20:10:28 +0100

    print

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,7 +11,7 @@
 int largest_number(int a, int b, int c)
 {
 
-	if (a >= b && a= > c)
+	if (a >= b && a >= c)
 	{
 		return (a);
 	}
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_remaining_dats - takes a date and prints how many days are 
+ * print_remaining_days - takes a date and prints how many days are 
  * left in the year, taking leap years into account
  * @month: month in number format
  * @day: day of month
@@ -18,6 +18,7 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
+
 
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 366 - day);
