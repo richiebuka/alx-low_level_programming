d2615fe80b86728044e3e6d3b0ba0e8c2d832696
richiebuka <rchiebuka@gmail.com>
2023-03-27 15:07:37 +0100

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - counts and returns string length
+ * *s: the string argument
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+	while (*s != 0)
+	{
+		counter++;
+		s++;
+	}
+	return (counter);
+}
