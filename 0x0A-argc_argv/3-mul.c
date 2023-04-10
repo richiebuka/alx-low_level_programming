bfdc0cae4b8e35500ba12927864956bfcf7e07b9
richiebuka <rchiebuka@gmail.com>
2023-04-10 17:06:05 +0100

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,12 +9,19 @@
  */
 int main(int argc, char *argv[])
 {
+	int num1, num2, prod;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	printf("%d\n", atoi(agrv[1]) * atoi(argc[2]));
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	print("%d\n", prod);
+
 	return (0);
 }
