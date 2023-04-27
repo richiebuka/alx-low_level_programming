d6b1e5f2b358d6a4e795cb7d9bfb3f2ef13e75d9
richiebuka <rchiebuka@gmail.com>
2023-04-27 12:25:00 +0100

    fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,7 +4,7 @@
  * @h: Pointer to a list
  * Return: Integer
  */
-size_t list_lens(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	const list_t *temp;
 	unsigned int counter = 0;
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 /**
- * add_node_end - add a new m=node at the end of a list
+ * add_node_end - add a new node at the end of a list
  * @head: Address of the first node of a list
  * @str: Address of the string to insert into the new node
  * Return: Address of the new node
- */
+ **/
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *temp, *temp2;
