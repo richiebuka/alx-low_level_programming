4270c3905099a701a8a5c1d4149503481a327b8f
richiebuka <rchiebuka@gmail.com>
2023-05-02 14:32:54 +0100

    fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (; i < idx - 1 && temp != NULL; i++)
 		temp = temp->next;
 	if (temp == NULL)
-		return (NULL):
+		return (NULL);
 	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
