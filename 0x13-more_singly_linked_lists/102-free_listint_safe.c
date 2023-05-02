e6a44eedbc216a3a8c641b062647cbb4a4d8d420
richiebuka <rchiebuka@gmail.com>
2023-05-02 15:21:23 +0100

    fix

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,23 +9,26 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
-	listint_t *temp = NULL;
+	int diff;
+	listint_t *temp;
 
-	if (!(h && *h))
-		return (len);
+	if (!h || !*h)
+		return (0);
+
 	while (*h)
 	{
-		len++;
-		if (*h > (*h)->next)
+		diff = *h - (*h)->next;
+		if (diff > 0)
 		{
-			temp = *h;
-			*h = (*h)->next;
-			free(temp);
+			temp = (*h)->next;
+			*h = temp;
+			len++;
 		}
 		else
 		{
-			free(*h);
 			*h = NULL;
+			len++;
+			break;
 		}
 	}
 	*h = NULL;
