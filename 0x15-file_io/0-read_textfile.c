fd43fbfa2f16a35edb8fa4229c45e034355447ce
richiebuka <rchiebuka@gmail.com>
2023-05-08 15:43:26 +0100

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,43 @@
-#include "main.h"
+#include "holberton.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - reads a certain amount of letters from a file
+ * @filename: name of file
+ * @letters: how many letters to read
+ * Return: the number of letters
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
-	char *buf;
-	if (!filename)
+	int fd, rv, wv;
+	char *buffer = malloc(sizeof(char) *letters);
+
+	if (buffer == NULL || filename == NULL)
 		return (0);
+
 	fd = open(filename, o_RDONLY);
 	if (fd < 0)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
-	buf[i] = '\0';
+	rv = read(fd, buffer, letters);
+	if (rv < 0)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	wv = write(STDOUT_FILENO, buffer, rv);
+	if (wv < 0 || wv != rv)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	free(buf);
-	return (y);
+	return (rv);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,29 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <elf.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdarg.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+/* prototypes for ELF */
+void errors(char *msg);
+void pentry(void);
+void ptype(char *buffer);
+void pos(char *buffer);
+void pversion(char *buffer);
+void pABIversion(char *buffer);
+void pdata(char *buffer);
+void pmagic(char *buffer);
+void pclass(char *buffer);
+void ptitle(char *title);
+int main(int argc, char **argv);
 
-/**
- * ELF header functions
-*/
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
-#endif
+#endif /* MAIN_H */
