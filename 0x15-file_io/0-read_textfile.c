83a955874898ee03eb6c8429faa82b6f9f1fc9f0
richiebuka <rchiebuka@gmail.com>
2023-05-08 16:16:14 +0100

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,37 @@
-#include "holberton.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include "main.h"
 /**
- * read_textfile - reads a certain amount of letters from a file
- * @filename: name of file
- * @letters: how many letters to read
- * Return: the number of letters
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, rv, wv;
-	char *buffer = malloc(sizeof(char) *letters);
-
-	if (buffer == NULL || filename == NULL)
+	int fd;
+	int i, y;
+	char *buf;
+	if (!filename)
 		return (0);
-
-	fd = open(filename, o_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd < 0)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	i = read(fd, buf, letters);
+	if (i < 0)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-	rv = read(fd, buffer, letters);
-	if (rv < 0)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-	wv = write(STDOUT_FILENO, buffer, rv);
-	if (wv < 0 || wv != rv)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
-	free(buffer);
+	buf[i] = '\0';
 	close(fd);
-	return (rv);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (y);
 }
